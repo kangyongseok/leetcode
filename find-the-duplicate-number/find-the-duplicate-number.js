bb828cf9ad2180e05966c097d25a62682c8d07f5
강용석 <kangyongsuek@gmail.com>
2021-05-18 05:24:27 +0900

    Time: 424 ms (5.05%), Space: 60.3 MB (7.49%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.js
+++ find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,16 +3,17 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-   // for (let i = 0; i < nums.length; i++) {
+   //1.  for (let i = 0; i < nums.length; i++) {
    //      for (let j = i + 1; j < nums.length; j++) {
    //          if (nums[i] === nums[j]) return nums[i]
    //      }
    // }
     
-    // return nums.find((num, i) => nums.indexOf(num) !== i)
+    //2.  return nums.find((num, i) => nums.indexOf(num) !== i)
     
     let indexJump = nums[0];
     let numJump = nums[0];
+    
     do {
         indexJump = nums[indexJump]
         numJump = nums[nums[numJump]]
@@ -20,10 +21,12 @@ var findDuplicate = function(nums) {
         indexJump = nums[0]
         
         while (indexJump !== numJump) {
+            console.log(indexJump, numJump)
             indexJump = nums[indexJump];
             numJump = nums[numJump];
         }
         
         return numJump
     }
+    
 };
