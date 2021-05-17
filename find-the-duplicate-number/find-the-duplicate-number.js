99f12adf2bbc5309b7a0db9e43fac295b8a8b7ea
강용석 <kangyongsuek@gmail.com>
2021-05-17 21:51:11 +0900

    Time: 7880 ms (5.03%), Space: 48.2 MB (25.48%) - LeetHub

---
+++ find-the-duplicate-number/find-the-duplicate-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let result = null
+   for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) return nums[i]
+        }
+   }
+};
