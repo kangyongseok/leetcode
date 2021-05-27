37f271e36a2928509db2f24e5dca0b4b11982d84
강용석 <kangyongsuek@gmail.com>
2021-05-27 21:30:39 +0900

    Time: 172 ms (51.51%), Space: 51.2 MB (15.89%) - LeetHub

--- daily-temperatures/daily-temperatures.js
+++ daily-temperatures/daily-temperatures.js
@@ -3,20 +3,30 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    const result = []
+    
+    const result = Array.from({length: temperatures.length}, () => 0);
+    const stack = [];
     for (let i = 0; i < temperatures.length; i++) {
-        if (i === temperatures.length - 1) {
-            result.push(0)
+        while(stack.length > 0 && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+            let j = stack.pop()
+            result[j] = i - j
         }
-        for (let j = i + 1; j < temperatures.length; j++) {
-            if (temperatures[i] < temperatures[j]) {
-                result.push(j - i)
-                break;
-            }
-            if (temperatures.length - 1 === j) {
-               result.push(0)
-            }
-        }
+        stack.push(i)
     }
+    console.log(result)
+    // for (let i = 0; i < temperatures.length; i++) {
+    //     if (i === temperatures.length - 1) {
+    //         result.push(0)
+    //     }
+    //     for (let j = i + 1; j < temperatures.length; j++) {
+    //         if (temperatures[i] < temperatures[j]) {
+    //             result.push(j - i)
+    //             break;
+    //         }
+    //         if (temperatures.length - 1 === j) {
+    //            result.push(0)
+    //         }
+    //     }
+    // }
     return result
 };
