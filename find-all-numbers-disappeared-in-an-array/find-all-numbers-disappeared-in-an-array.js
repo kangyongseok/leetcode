c4268cefcb18a4d3b2ecee58b64961731aa4a94c
강용석 <kangyongsuek@gmail.com>
2021-06-11 23:07:58 +0900

    Time: 6620 ms (21.12%), Space: 46.4 MB (86.34%) - LeetHub

---
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    const result = []
+    for (let i = 0; i < nums.length; i++) {
+        if (!nums.includes(i + 1)) {
+            result.push(i + 1)
+        }
+    }
+    return result
+};
